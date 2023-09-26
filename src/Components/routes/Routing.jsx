9a7df16ac078686f11736cbc564a7923d8bfb833
Adarsha A <adarshaadi1997@gmail.com>
2023-09-26 18:40:37 +0530

    prevent default

--- src/Components/routes/Routing.jsx
+++ src/Components/routes/Routing.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import FeedbackForm from '../Feedback/FeedbackForm';
-import About from '../About/About';
-import Home from '../Home/Home';
-import Blog from '../Blog/Blog';
+import FeedbackForm from "../Feedback/FeedbackForm";
+import About from "../About/About";
+import Home from "../Home/Home";
+import Blog from "../Blog/Blog";
 
 const Routing = () => {
   return (
     <>
-       <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<About />} />
         <Route path="/feedback" element={<FeedbackForm />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default Routing
+export default Routing;
