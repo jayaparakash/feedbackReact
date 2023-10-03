1a9f602dee655fb8a9cbcd9b7a880b1b8fe23f7e
jayaparakash <korlakuntajp1403@gmail.com>
2023-10-03 13:33:34 +0530

    added carousel

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,48 +1,49 @@
 import React from "react";
-
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from "../../Images/IMG_20230327_132504507.jpg"
+import img2 from '../../Images/IMG_20230327_133656755.jpg';
+import img3 from '../../Images/IMG_20230327_132504507.jpg';
 const Blog = () => {
   return (
-    <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. At reiciendis
-      corporis perferendis, laborum illum tempora assumenda officia sequi eum
-      aspernatur maxime nesciunt hic dolore dignissimos aut laudantium
-      voluptatibus eveniet harum officiis delectus commodi, quos natus obcaecati
-      magnam! Nisi soluta corrupti expedita, nesciunt quibusdam maxime quae
-      placeat aperiam cupiditate autem nulla. Molestiae non eligendi, sit alias
-      totam temporibus esse laudantium consectetur, dolorum repellendus quisquam
-      dolor sint assumenda praesentium culpa ipsa expedita excepturi accusamus
-      velit nobis fugit fuga cum! Delectus dicta saepe, repellat ipsum impedit
-      quaerat sapiente quia itaque quos, voluptatem harum culpa praesentium
-      distinctio officia ad, eaque a exercitationem ducimus nam sunt magni
-      dolore modi assumenda odit? Quis doloribus similique, quia cum, cupiditate
-      deleniti tempora reiciendis eaque culpa sequi, nam ducimus molestias
-      consequuntur minus vel dignissimos natus laboriosam. Soluta quisquam
-      possimus, neque nemo quibusdam consectetur tenetur. Eligendi asperiores
-      nostrum omnis blanditiis eius minima fuga, quibusdam distinctio hic enim
-      quam voluptatibus obcaecati molestiae harum culpa quod ad provident
-      deserunt corporis at vero est natus sint. Voluptate, voluptates natus quia
-      aspernatur fugit deleniti necessitatibus tenetur esse assumenda
-      voluptatibus! In consequuntur assumenda, recusandae facilis impedit
-      deserunt nihil excepturi accusantium odio quibusdam quas ratione
-      praesentium voluptates! Enim numquam mollitia nemo amet laboriosam, harum
-      maiores possimus illum nulla sit incidunt ipsa provident impedit quod
-      consectetur aut unde dolore laborum deleniti, autem, soluta doloremque
-      doloribus eligendi omnis. Ex est ipsa quia corrupti? Magni, aliquam saepe?
-      Asperiores nisi facilis repellat! Assumenda sequi quas, culpa adipisci
-      praesentium fuga, dolorem eos cum dignissimos deleniti autem soluta
-      excepturi voluptates delectus veniam aut, obcaecati libero expedita
-      commodi et qui facilis consequatur! Tenetur magni voluptate quo reiciendis
-      animi quae hic doloremque laudantium illo consectetur. Asperiores
-      exercitationem sunt cum alias distinctio suscipit facere placeat. Ducimus,
-      ipsum saepe. Architecto voluptatem vero, est maiores rem odit eaque quae
-      officiis impedit voluptatum ipsa praesentium saepe libero earum sint.
-      Voluptas eos, error nam praesentium nesciunt iste voluptates porro aut
-      assumenda iusto eum at, dignissimos harum necessitatibus consectetur
-      deleniti ab beatae alias suscipit? Minus reprehenderit dolor laborum, odit
-      itaque ex. Ipsam, tempora aperiam quo eum veritatis deleniti reiciendis
-      beatae tenetur eos facere, magni laboriosam pariatur porro similique
-      reprehenderit dignissimos?
-    </div>
+    <div className="carousel">
+    <Carousel data-bs-theme="dark">
+   <Carousel.Item>
+     <img
+       className="d-block w-100"
+       src={img1}
+       alt="First slide"
+     />
+     <Carousel.Caption>
+       <h5>First slide label</h5>
+       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+     </Carousel.Caption>
+   </Carousel.Item>
+   <Carousel.Item>
+     <img
+       className="d-block w-100"
+       src={img2}
+       alt="Second slide"
+     />
+     <Carousel.Caption>
+       <h5>Second slide label</h5>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+     </Carousel.Caption>
+   </Carousel.Item>
+   <Carousel.Item>
+     <img
+       className="d-block w-100"
+       src={img3}
+       alt="Third slide"
+     />
+     <Carousel.Caption>
+       <h5>Third slide label</h5>
+       <p>
+         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+       </p>
+     </Carousel.Caption>
+   </Carousel.Item>
+ </Carousel>        
+     </div>
   );
 };
 
